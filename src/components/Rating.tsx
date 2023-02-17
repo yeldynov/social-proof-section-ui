0ae2d91f09317a1ebea89cf2903f90c7a3fd2392
Nico Yeldynov <yeldynov@gmail.com>
2023-02-17 16:15:45 +0200

    fixed rating flexbox

--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export default function Rating({ source, rating }: Props) {
   return (
-    <div className='mob:first:-ml-10 mob:first:mr-10 mob:last:-mr-10 mob:last:ml-10  flex flex-col items-center justify-center bg-gray-magenta-light rounded-xl p-4 mb-4'>
+    <div className='mob:first:-ml-10 mob:first:mr-10 mob:last:-mr-10 mob:last:ml-10 mob:flex-row mob:gap-5 flex flex-col items-center justify-center bg-gray-magenta-light rounded-xl p-4 mb-4'>
       <div className='flex gap-2 mb-2'>
         {[...Array(rating)].map((e, i) => (
           <span>
