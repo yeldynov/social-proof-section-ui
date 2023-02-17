0312188f5422a3952486b471f4171b202c8d5cf3
Nico Yeldynov <yeldynov@gmail.com>
2023-02-17 16:17:04 +0200

    increased title font

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ type Props = {};
 export default function Header({}: Props) {
   return (
     <div className='mob:text-left my-12 flex flex-col gap-5 justify-center text-center'>
-      <h1 className=' mob:w-[50%] text-[2.5rem] leading-8 tracking-tight font-[700] mx-4 text-magenta-dark'>
+      <h1 className=' mob:w-[50%] text-[2.5rem] mob:text-[3rem] leading-8 tracking-tight font-[700] mx-4 text-magenta-dark'>
         10,000+ of our users love our products.
       </h1>
       <p className='mob:w-[70%] text-lg mx-2 text-gray-magenta tracking-tight font-[500]'>
